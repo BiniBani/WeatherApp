5a62ec964a8dc290821e4c2cfb142c7c3fd682f7
BiniBani <zhizhujin@naver.com>
2022-07-02 20:50:19 +0900

    primarySwatch is changed to Colors.blue

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Weather app',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: Loading(),
     );
